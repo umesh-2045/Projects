130818d23661ae004dd56432e78ba4473e1c73c7
Umesh Jagtap <umeshtejas2004@gmail.com>
2025-04-09 16:19:05 +0000

    abhi bohot baki he

---
+++ Megablog/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+console.log(process.env.REACT_APP_APPWRITE_URL);
+
+
+  return (
+    <>
+      <h1>A blog app in appwrite</h1>
+    </>
+  )
+}
+
+export default App
